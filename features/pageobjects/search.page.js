fa67e7e94ed6f2bf79af7f2325ffdf62aba2f513
Tania <47900977+txt2021@users.noreply.github.com>
2022-10-21 11:51:14 +0300

    Update search.page.js

--- features/pageobjects/search.page.js
+++ features/pageobjects/search.page.js
@@ -11,46 +11,46 @@ const devops_text = "What is DevOps?";
 const gitlab_link = ('a[href="https://gitlab.com/users/sign_in/"]');
 
 
-class SearchPage { 
+class SearchPage extends page { 
 
         async enterSearchItem(){
-           await page.setvalue(search_input,search_text);
+           await this.setvalue(search_input,search_text);
         }
 
         async clickGitlabButton(){
-           await page.click(gitlab_link);            
+           await this.click(gitlab_link);            
         }
 
         async clickdevopsButton(){
-           await page.click(devops_button);        
+           await this.click(devops_button);        
         }
 
         async isresulttitleVisible(){
-           await (expect(page.isdisplayed(searh_results_title)));
+            await this.isdisplayed(searh_results_title);
         }
 
         async isresulttitleCorrect(){
-            await (expect(page.checkElement(searh_results_title,search_text)));
+            await this.checkElement(searh_results_title,search_text);
         }
         
         async isdevopsVisible(){
-            await (expect(page.isdisplayed(devops_title)));
+            await this.isdisplayed(devops_title);
           }
   
         async isdevopsCorrect(){
-            await (expect(page.checkElement(devops_title,devops_text)));
+            await this.checkElement(devops_title,devops_text);
         }
 
         async urlAboutChecking(){
-            await (expect(page.urlChecking('about')));
+            await this.urlChecking('about');
         }
 
         async urlDevopsChecking(){
-            await (expect(page.urlChecking('devops')));
+            await this.urlChecking('devops');
         }
 
         async urlSigninChecking(){
-            await (expect(page.urlChecking('sign_in')));
+            await this.urlChecking('sign_in');
         }
        
 }
